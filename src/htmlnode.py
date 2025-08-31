626d58c7e6ff2abae9c92c2103df70f1012ed9ad
unknown <joseph.harley02@gmail.com>
2025-08-31 12:10:13 -0400

    add leaf node subclass and added unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,5 +17,16 @@ class HTMLNode():
         return props_html
     
     def __repr__(self):
-        return f"HTMLNode{self.tag, self.value}, children: {self.children}, {self.props}"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
     
